d7d5a0c07ea9eff81069b95f35fe7d5db71c7579
Igor Mihajlov <igor@Igors-MacBook-Pro.local>
2023-12-22 00:25:20 +0100

    Use command line option library

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,48 @@
 #include <note_manager.hpp>
 
 #include <fmt/core.h>
+#include <fmt/color.h>
 #include <cxxopts.hpp>
 
 #include <algorithm>
 #include <string>
+#include <exception>
 
-static auto parse_options(const int argc, const char* argv[])
+static auto options = cxxopts::Options("jotter", "Command line utility for managing notes");
+
+[[nodiscard]] 
+static auto init_options(
+    const int argc, 
+    const char* argv[], 
+    [[maybe_unused]] jotter::config& cfg
+    )
 {
-    auto options = cxxopts::Options("jotter", "Command line utility for managing notes");
     options.add_options()
-    ("n,note", "Note to write", cxxopts::value<std::string>());
+    ("n,note", "Note to write", cxxopts::value<std::string>())
+    ("h,help", "Print usage");
 
-    return options.parse(argc, argv);
+    return options.parse(argc, argv);;
 }
 
-int main(const int argc, const char* argv[]) try
-{
-/*    if(argc < 2)
-    {
-        fmt::println("Nothing no jot, please enter a note like so:");
-        fmt::println("\t{} \"<your note>\"", argv[0]);
-        return EXIT_FAILURE;
-    }*/
-
-    auto parsed_options = parse_options(argc, argv);
-    
-    fmt::println("{}", parsed_options["note"].as<std::string>());
-
+int main(
+    const int argc, 
+    const char* argv[]
+    ) try
+{   
     auto config = jotter::get_config();
-    jotter::record_note(argv[1], config);
+    auto parsed_options = init_options(argc, argv, config);
+    if(parsed_options.count("help"))
+    {
+        fmt::println("{}", options.help());
+        return EXIT_SUCCESS;
+    }
+
+    auto note = parsed_options["note"].as<std::string>();
+    jotter::record_note(note, config);
 
     return EXIT_SUCCESS;
 }
 catch(const std::exception& e)
 {
-    fmt::println("Exception caught: {}.", e.what());
+    fmt::print(fg(fmt::color::crimson), "{}\n", e.what());
 }
