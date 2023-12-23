93aa2bced3378ad0a409fe3e06f307bae47fc391
Igor Mihajlov <igor@Igors-MacBook-Pro.local>
2023-12-24 00:33:25 +0100

    Add support in config to create file and directories if they do not exist

--- src/config.cpp
+++ src/config.cpp
@@ -5,23 +5,42 @@
 #include <nlohmann/json.hpp>
 
 #include <fstream>
+#include <filesystem>
 
 namespace jotter
 {
-    static config get_default_config()
+    inline static config get_default_config() noexcept
     {
         static constexpr auto default_notes_location = "/jotter/notes.txt";
         return {.notes_location = default_notes_location};
     }
 
-    [[nodiscard]] config get_config()
+    static auto create_config(std::string_view config_path)
     {
-        static constexpr auto config_location = "jotter-config.json";
-        auto config_file                      = get_home_location() + config_location;
-        auto file                             = std::ifstream(config_file);
+        std::filesystem::create_directories(std::filesystem::path(config_path).parent_path());
+        std::ofstream file(config_path);
         if(not file.is_open())
         {
-            throw std::runtime_error(fmt::format("Could not open config file {}", config_file));
+            throw std::runtime_error(fmt::format("Could not create config file {}", config_path));
+        }
+
+        file << "{}\n";
+        file.close();
+    }
+
+    [[nodiscard]] config get_config()
+    {
+        static constexpr auto config_location = ".config/jotter/jotter-config.json";
+        auto config_path                      = get_home_location() + config_location;
+        if(not std::filesystem::exists(config_path))
+        {
+            create_config(config_path);
+        }
+
+        auto file = std::ifstream(config_path);
+        if(not file.is_open())
+        {
+            throw std::runtime_error(fmt::format("Could not open config file {}", config_path));
         }
 
         nlohmann::json config_json;
